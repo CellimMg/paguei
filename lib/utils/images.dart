906a32e4a0c75917b1ff7131d1969f1d3824451b
Marcelo Luciano <cello.ma16@gmail.com>
2023-03-09 20:04:21 -0300

    [feat] added images for splash screen

--- lib/utils/images.dart
+++ lib/utils/images.dart
@@ -2,5 +2,5 @@ class ImagesRepository{
   static const String _imagesPath = 'assets/images';
   static const String authImage = '$_imagesPath/auth_image.png';
   static const String iconGoogle = '$_imagesPath/icon_google.png';
-
+  static const String splashImage = '$_imagesPath/splash-image.png';
 }
